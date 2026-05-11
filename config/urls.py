a73b93c5fcc97a80f68082333f8c12c3423cd820
Hemish123 <consultant@jmsadvisory.in>
2026-05-11 14:12:06 +0530

    Fix media serving in production

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,9 @@ urlpatterns = [
     path('api/templates/inventory-template/', InventoryTemplateView.as_view(), name='inventory-template'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.urls import re_path
+from django.views.static import serve
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
